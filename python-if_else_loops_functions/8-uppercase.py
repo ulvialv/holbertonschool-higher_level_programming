891c8e7e838349fd3ddf0a7f60b8da3561c074da
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-14 19:34:06 +0000

    asdas

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        # Kiçik hərfləri böyük hərflərə çeviririk
+        if ord('a') <= ord(c) <= ord('z'):
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
