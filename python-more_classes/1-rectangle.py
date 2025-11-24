67bd18d0ff746749748f1a98e46b5e40e3ce07c3
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-24 13:14:40 +0000

    asd

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module defines a Rectangle class."""
 
+
 class Rectangle:
     """Rectangle class that defines width and height."""
-    
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
