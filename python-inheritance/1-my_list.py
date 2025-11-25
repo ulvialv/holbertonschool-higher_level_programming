d1d16afa923afca428946d9d1b87e93eea171471
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-25 13:34:24 +0000

    asd

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,7 @@
 class MyList(list):
     """A class that inherits from list and prints the list sorted."""
 
+
     def print_sorted(self):
         """Prints the list in ascending sorted order."""
         print(sorted(self))
