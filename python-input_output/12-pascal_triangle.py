41e6d170c583470e18053a380bee12d366089d89
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-26 17:49:39 +0000

    asd

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module for generating Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle
+
+    Args:
+        n (int): Number of rows in the triangle
+
+    Returns:
+        list: List of lists representing Pascal's triangle
+              Empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
