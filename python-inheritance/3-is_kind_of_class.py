db0016bc0b56527891abc523ed89838f6002f696
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-26 11:52:18 +0000

    asd

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that checks if object is instance of or inherits from a_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is instance of or inherits from a_class"""
+    return isinstance(obj, a_class)
