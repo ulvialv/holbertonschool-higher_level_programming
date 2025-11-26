fd996867c10471a3ec667502c5aa25134197e884
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-26 17:47:50 +0000

    asd

---
+++ python-input_output/10-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
+class Student:
+    """
+    A class that defines a student
+
+    Attributes:
+        first_name (str): First name of the student
+        last_name (str): Last name of the student
+        age (int): Age of the student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance
+
+        Args:
+            first_name (str): First name of the student
+            last_name (str): Last name of the student
+            age (int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): List of attribute names to retrieve.
+                         If None, retrieve all attributes.
+
+        Returns:
+            dict: Dictionary containing specified attributes
+        """
+        if attrs is None:
+            return self.__dict__
+
+        filtered_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_dict[attr] = getattr(self, attr)
+        return filtered_dict
