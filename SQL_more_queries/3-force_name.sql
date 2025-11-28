19757b14af5022081bd95aa6c62cebcbf2ab6d7c
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 19:35:37 +0000

    asd

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,7 @@
+-- Creates table force_name in the selected database
+-- Database name will be given as argument when executing
+
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL    -- name field cannot be NULL
 );
