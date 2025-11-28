4fe792549b2af1499ad66a208da18a2ab44be49f
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 19:43:14 +0000

    asd

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Lists all shows that have at least one genre linked
+-- Each record: tv_shows.title - tv_show_genres.genre_id
+-- Results sorted by tv_shows.title ASC, then tv_show_genres.genre_id ASC
+-- Database name will be passed as argument to mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
