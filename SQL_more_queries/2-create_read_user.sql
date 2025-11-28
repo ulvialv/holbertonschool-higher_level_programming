7cc85df18fa4b468a81ab646fba53947f79e41fb
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 19:33:02 +0000

    sd

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
