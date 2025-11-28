60d9aab1f897c242c54dbf24dafa5be42159b413
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 19:25:57 +0000

    ads

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,10 @@
 -- Creates the user 'user_0d_1' if it does not already exist, and sets the passwords.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost IDENTIFIET BY 'user_0d_1_pwd';
 
+
 -- Grants ALL PRIVILEGES globally to the user, with the ability to grant those privileges to others.
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
 
 -- Reloads the grant tables to ensure the changes take effect immediately.
 FLUSH PRIVILEGES;
