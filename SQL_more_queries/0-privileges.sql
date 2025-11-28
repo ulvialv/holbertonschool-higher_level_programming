c7acbb953864a20e669fa565428dbb16257467d8
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 12:02:30 +0000

    asd

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges for user_0d_1 on the local server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Lists all privileges for user_0d_2 on the local server
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
