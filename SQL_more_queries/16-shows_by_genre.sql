74c6a670df9216d3a2da6558c0448e1605b3f1fd
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 19:57:43 +0000

    asd

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,17 @@
+-- Lists all shows and their genres (or NULL if no genre exists), sorted by title and genre name.
+SELECT
+    tv_shows.title,
+    tv_genres.name
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres
+ON
+    tv_shows.id = tv_show_genres.show_id
+LEFT JOIN
+    tv_genres
+ON
+    tv_show_genres.genre_id = tv_genres.id
+ORDER BY
+    tv_shows.title ASC,
+    tv_genres.name ASC;
