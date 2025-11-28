42ef99540a81d8634e216e791ede9f890f637ff4
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 19:55:58 +0000

    sad

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,17 @@
+-- Lists all shows that have 'Comedy' genre linked, sorted by show title ascending.
+SELECT
+    tv_shows.title
+FROM
+    tv_shows
+INNER JOIN
+    tv_show_genres
+ON
+    tv_shows.id = tv_show_genres.show_id
+INNER JOIN
+    tv_genres
+ON
+    tv_show_genres.genre_id = tv_genres.id
+WHERE
+    tv_genres.name = 'Comedy'
+ORDER BY
+    tv_shows.title ASC;
