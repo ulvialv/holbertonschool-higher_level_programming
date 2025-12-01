cdba6a84e1aab74dcd9d087c417873080f07ac78
ulvialv <ulvialiyevgithub@gmail.com>
2025-12-01 11:11:25 +0000

    ads

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using requests"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    content = response.text  # already decoded to str
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
