c014672276ec5f87f93972054e1c8182d22fd255
ulvialv <ulvialiyevgithub@gmail.com>
2025-12-02 11:17:12 +0000

    asd

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,24 +6,35 @@ import csv
 API_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
-response = requests.get(API_URL)
-
-print(f"Status code: {response.status_code}")
-
-if response.status_code != 200:
-    return
-
-data = response.json()
-
-for post in data:
-    print(post.get("title"))
-
+def fetch_and_print_posts():
+    """Fetch posts and print status + titles"""
     response = requests.get(API_URL)
+    
+    # Print status code
+    print(f"Status code: {response.status_code}")
+
+    if response.status_code != 200:
+        return
+
+    #parse Json
+
+    data = response.json()
+
+    # Print all titles
+    for post in data:
+        print(post.get("title"))
+def fetch_and_save_posts():
+    """Fetch posts and save to posts.csv."""
+    response = requests.get(API_URL)
+    
+
     if response.status_code != 200:
         return
 
     data = response.json()
 
+    
+    # Prepare data: list of dicts with id, title, body
     clean_data = [
             {
                 "id": post.get("id"),
@@ -32,6 +43,9 @@ for post in data:
             }
             for post in data
     ]
+    
+
+    # Write CSV
     with open("posts.csv", "w", newline="", encoding="utf-8") as f:
 
         writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
